ec4f009578ec9b3997e20f89c921f16def32be82
AMG <vitos162008@yandex.ru>
2023-09-07 07:30:36 +0300

    action test

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
+// логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
